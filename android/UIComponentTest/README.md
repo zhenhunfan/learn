07529a6b5ad66290c41a1fadb410e806c6c5b0ef
zhenhunfan <zhang88hong34@126.com>
2020-11-09 10:18:09 +0800

    添加UI组件演示项目

---
+++ android/UIComponentTest/README.md
@@ -0,0 +1,23 @@
+该项目演示UI组件
+
+1. 常用的ui组件
+   1. TextView
+   2. ToggleButton
+   3. EditText
+   4. Button
+   5. CheckBox
+   6. RadioButton + RadioGroup
+   7. ImageView
+   8. ...
+2. 进度条组件ProgressBar的使用
+   1. 圆形进度条
+   2. 水平进度条
+3. 对话框AlertDialog的使用
+   1. 普通对话框
+   2. 单选/复选对话框
+   3. 自定义对话框
+   4. 进度条对话框
+   5. 日期选择对话框
+4. 常用组件的综合应用
+   1. 注册窗口
+   2. 调用摄像头获取照片
---
+++ android/UIComponentTest/app/src/main/java/com/zhenhunfan/ui/component/test/MainActivity.java
@@ -0,0 +1,85 @@
+package com.zhenhunfan.ui.component.test;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 打开normal对话框
+     * @param view
+     */
+    public void startNormalDialog(View view){
+        new AlertDialog.Builder(this)
+                .setTitle("普通对话框")
+                .setMessage("对话框内容")
+                .setCancelable(true)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "点击了确定按钮", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "点击了取消按钮", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * 打开单选/复选对话框
+     * @param view
+     */
+    public void startListDialog(View view){
+        final String[] colors = {"Green","Blue","Red","Yellow"};
+        new AlertDialog.Builder(this)
+                .setTitle("单选/复选对话框")
+                .setCancelable(false)
+                .setSingleChoiceItems(colors, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, colors[which], Toast.LENGTH_SHORT).show();
+                    }
+                })
+                //.setMultiChoiceItems()  设置复选对话框
+                .setPositiveButton("确定", null)
+                .show();
+    }
+
+    /**
+     * 打开自定义对话框
+     * @param view
+     */
+    public void startCustomDialog(View view){
+        View v = View.inflate(this,R.layout.custom_dialog,null);
+        final EditText et_username = v.findViewById(R.id.et_username);
+        final EditText et_pwd = v.findViewById(R.id.et_pwd);
+
+        new AlertDialog.Builder(this)
+                .setTitle("单选/复选对话框")
+                .setCancelable(false)
+                .setView(v)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "username:"+et_username.getText()+" pwd:"+et_pwd.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+}
